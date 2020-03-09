b582690f683876bb1d3c5fbc01832922205b4f24
reggie <reggiegunawan88@gmail.com>
2020-03-09 19:34:25 +0700

    Import login to App.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,66 +1,92 @@
-import React, {Component} from 'react';
-import {NavigationActions} from 'react-navigation';
+import React from 'react';
 import {
-  View,
-  Text,
   StyleSheet,
+  Text,
+  View,
   TextInput,
   TouchableOpacity,
 } from 'react-native';
 
-export default class Login extends Component {
-  login() {
-    var {dispatch, navigate} = this.props.navigation;
-    const reset = NavigationActions.reset({
-      index: 0,
-      key: null,
-      actions: [NavigationActions.navigate({routeName: 'StackOverTabs'})],
-    });
-    dispatch(reset);
-  }
+export default class App extends React.Component {
+  state = {
+    email: '',
+    password: '',
+  };
+
   render() {
     return (
-      <View behavior="padding" style={styles.Wrapper}>
-        <TextInput
-          placeholder="email"
-          underlineColorAndroid="white"
-          placeholderTextColor="white"
-          keyboardType="email-address"
-          style={styles.inputField}
-        />
-        <TextInput
-          placeholder="password"
-          underlineColorAndroid="white"
-          placeholderTextColor="white"
-          secureTextEntry={true}
-          style={styles.inputField}
-        />
-        <TouchableOpacity onPress={this.login.bind(this)}>
-          <Text style={{color: 'white', marginTop: 10}}>Login</Text>
+      <View style={styles.container}>
+        <Text style={styles.logo}>e-Puskesmas</Text>
+        <View style={styles.inputView}>
+          <TextInput
+            style={styles.inputText}
+            placeholder="Username..."
+            placeholderTextColor="#003f5c"
+            onChangeText={text => this.setState({email: text})}
+          />
+        </View>
+        <View style={styles.inputView}>
+          <TextInput
+            secureTextEntry
+            style={styles.inputText}
+            placeholder="Password..."
+            placeholderTextColor="#003f5c"
+            onChangeText={text => this.setState({password: text})}
+          />
+        </View>
+        <TouchableOpacity>
+          <Text style={styles.forgot}>Forgot Password?</Text>
         </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() => this.props.navigation.navigate('signup')}>
-          <Text style={{color: 'white', marginTop: 10}}>SignUp</Text>
+        <TouchableOpacity style={styles.loginBtn}>
+          <Text style={styles.loginText}>LOGIN</Text>
         </TouchableOpacity>
       </View>
     );
   }
 }
+
 const styles = StyleSheet.create({
-  inputField: {
-    width: 280,
-    color: 'white',
-    borderColor: 'white',
-    marginTop: 5,
-  },
-  Wrapper: {
+  container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#44c503',
     alignItems: 'center',
-    backgroundColor: '#1F3A93',
+    justifyContent: 'center',
   },
-  text: {
-    color: 'blue',
-    fontSize: 23,
+  logo: {
+    fontWeight: 'bold',
+    fontStyle: 'italic',
+    fontSize: 50,
+    color: 'white',
+    marginBottom: 40,
+  },
+  inputView: {
+    width: '80%',
+    backgroundColor: 'white',
+    borderRadius: 25,
+    height: 50,
+    marginBottom: 20,
+    justifyContent: 'center',
+    padding: 20,
+  },
+  inputText: {
+    height: 50,
+    color: 'black',
+  },
+  forgot: {
+    color: 'white',
+    fontSize: 11,
+  },
+  loginBtn: {
+    width: '80%',
+    backgroundColor: '#fb5b5a',
+    borderRadius: 25,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 40,
+    marginBottom: 10,
+  },
+  loginText: {
+    color: 'white',
   },
 });
